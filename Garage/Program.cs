6714bea1fc76d93fb0b1abc8f1150cf885bc90a0
Diana Todorova <diana.n.todorova@gmail.com>
2025-06-16 11:19:47 +0200

    Add project files.

---
+++ Garage/Program.cs
@@ -0,0 +1,15 @@
+﻿using Garage.Interfaces;
+using Garage.UI;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace Garage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IUI ui = new ConsoleUI();
+            ui.StartGarage();
+        }
+    }
+}
---
+++ Garage/UI/ConsoleUI.cs
@@ -0,0 +1,41 @@
+﻿using Garage.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage.UI
+{
+    internal class ConsoleUI : IUI
+    {
+        public void StartGarage()
+        {
+            while (true) 
+            {
+                Console.WriteLine("Welcome to The City Parking ");
+                Console.WriteLine("\nMake a choice:\n1 Create a new garage \n2. Park a car \n3. Leave the parking \n4. Show all vehicels \n5. Search for vehicle \n6. Exit!");
+                string choice= Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        Console.WriteLine("Enter a registration number:");
+                        string regNumber= Console.ReadLine().ToUpper();
+                        break;
+                    case "6":
+                        break;
+
+                }
+            
+            }
+        }
+    }
+}
